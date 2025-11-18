de9e2d3d66cbeef8a62589bb3068a6718168daaa
rdas3-bths <98552214+rdas3-bths@users.noreply.github.com>
2025-11-18 14:12:35 -0500

    Add files via upload
    11/18 commit

--- Main.java
+++ Main.java
@@ -10,17 +10,28 @@ public class Main {
 
         Scanner s = new Scanner(System.in);
 
+        int tries = 0;
         while (!playerCheck) {
+            tries++;
+            System.out.println("---------------------");
             System.out.print("Press Enter to roll!");
             s.nextLine();
             playerCheck = DiceWars.playerRoll(goal);
             if (playerCheck) {
-                System.out.println("Player roll wins!");
+                System.out.println("Got it!");
             }
             else {
-                System.out.println("Player roll loses!");
+                System.out.println("Did not get the goal number! ("+goal+")");
             }
         }
+
+        if (tries == 1) {
+            System.out.println("It took you " + tries + " try to win!");
+        }
+        else {
+            System.out.println("It took you " + tries + " tries to win!");
+        }
+
 
 
         // two player game
