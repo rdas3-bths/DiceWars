54cedc9a8f5ae4d1cf895d0aa387d0f599d7e60c
rdas3-bths <98552214+rdas3-bths@users.noreply.github.com>
2025-11-20 13:38:49 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -4,26 +4,55 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Welcome to Dice Wars");
-        int goal = DiceWars.getGoal();
-        System.out.println("Goal is: " + goal);
 
-        System.out.println("Player 1 goes first!");
-        int playerOneTries = DiceWars.doPlayerRound(goal);
+        Scanner s = new Scanner(System.in);
+        System.out.print("How many rounds would you like to play? ");
+        int rounds = Integer.parseInt(s.nextLine());
 
-        System.out.println("Player 1 round ends");
+        int currentRound = 1;
+        int playerOneWins = 0;
+        int playerTwoWins = 0;
+        while (currentRound <= rounds) {
+            System.out.println("Current Round: " + currentRound);
+            System.out.println("Scoreboard:");
+            System.out.println("+-----------------------------+");
+            System.out.println("|   Player one: " + playerOneWins + "             |");
+            System.out.println("|   Player two: " + playerTwoWins + "             |");
+            System.out.println("+-----------------------------+");
+            int goal = DiceWars.getGoal();
+            System.out.println("Goal is: " + goal);
 
-        System.out.println("Player 2 next!");
-        int playerTwoTries = DiceWars.doPlayerRound(goal);
 
-        if (playerOneTries > playerTwoTries) {
-            System.out.println("Player two wins!");
+
+            System.out.println("Player 1 goes first!");
+            int playerOneTries = DiceWars.doPlayerRound(goal);
+
+            System.out.println("Player 1 round ends");
+
+            System.out.println("Player 2 next!");
+            int playerTwoTries = DiceWars.doPlayerRound(goal);
+
+            if (playerOneTries > playerTwoTries) {
+                System.out.println("Player two wins!");
+                playerTwoWins++;
+            }
+            else if (playerTwoTries > playerOneTries) {
+                System.out.println("Player one wins!");
+                playerOneWins++;
+            }
+            else {
+                System.out.println("It's a tie!");
+            }
+            currentRound++;
         }
-        else if (playerTwoTries > playerOneTries) {
-            System.out.println("Player one wins!");
-        }
-        else {
-            System.out.println("It's a tie!");
-        }
+
+        System.out.println("FINAL SCORE: " + currentRound);
+        System.out.println("Scoreboard:");
+        System.out.println("+-----------------------------+");
+        System.out.println("|   Player one: " + playerOneWins + "             |");
+        System.out.println("|   Player two: " + playerTwoWins + "             |");
+        System.out.println("+-----------------------------+");
+
 
         // two player game
         // when the round starts, two dice are rolled
